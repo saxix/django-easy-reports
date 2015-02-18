563487891d79b935d3d23016182e3617c573d58d
mrc75 <marcovittorini@gmail.com>
2015-02-18 15:15:26 +0100

    fixes compatibility

--- ereports/forms.py
+++ ereports/forms.py
@@ -30,4 +30,19 @@ class ReportConfigurationForm(ModelForm):
 
     class Meta:
         model = ReportConfiguration
-        fields = '__all__'
+        fields = (
+            'name',
+            'group',
+            'template',
+            'description',
+            'report_class',
+            'target_model',
+            'published',
+            'select_related',
+            'use_distinct',
+            'columns',
+            'filtering',
+            'ordering',
+            'groupby',
+            'cache_key',
+        )
